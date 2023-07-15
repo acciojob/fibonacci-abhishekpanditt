932004a74abe99bd35a5b7d6da1cfdce7dc646ae
abhishekpanditt <123313123+abhishekpanditt@users.noreply.github.com>
2023-07-15 16:12:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
-// your code here
+if(num === 1)
+	{
+		return 0;
+	}
+	else if(num === 2)
+	{
+		return 1;
+	}
+    
+	return fibonacci(num-1) + fibonacci(num-2);
 }
 
 module.exports = fibonacci;
